e1f3c0870cfd7fcaad52c48c79c8ceeea6f2d98a
Adarsh Ranjan <adarshranjan.4890@gmail.com>
2026-02-03 15:29:50 +0530

    Create Req 1.sql

---
+++ SQL - Queries /Req 1.sql
@@ -0,0 +1,17 @@
+### 1️⃣ Product-Level Sales Performance
+
+**Objective:** Monthly product sales for Amazon India (FY 2021).
+
+```sql
+SELECT 
+    f.date,
+    p.product,
+    p.variant,
+    SUM(f.sold_quantity) AS total_qty,
+    SUM(f.sold_quantity * f.gross_price) AS gross_revenue
+FROM fact_sales_monthly f
+JOIN dim_products p ON f.product_code = p.product_code
+JOIN dim_customers c ON f.customer_code = c.customer_code
+WHERE c.customer = 'Amazon India'
+  AND f.fiscal_year = 2021
+GROUP BY f.date, p.product, p.variant;
